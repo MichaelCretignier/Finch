346c8bb996bcf180a635786f9e4bb4477db2d6b6
MichaelCretignier <44569244+MichaelCretignier@users.noreply.github.com>
2026-02-14 08:53:16 +0000

    update the versioning number to 2.0.5

--- finch/Finch.py
+++ finch/Finch.py
@@ -17,7 +17,7 @@ from . import Finch_GP as fgp
 from . import Finch_functions as ff
 from . import Finch_variables as fv
 
-__version__ = '2.0.3'
+__version__ = '2.0.5'
 
 print(Fore.GREEN+"""\n[INFO FINCH]
 [INFO USER] FINCH version = """+__version__ +""" 
